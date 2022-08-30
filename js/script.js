95b5d4cb0cac78bab415bd29c939d4c4184e4a27
Ilman Eldarov Ash-shishani <ilmaneldarov@gmail.com>
2022-08-30 16:42:21 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,44 +1,249 @@
-const author = {
-miki: 45,
-mango: 12,
-kiwi: 10,
-anton: 8,
+// const author = {
+// miki: 45,
+// mango: 12,
+// kiwi: 10,
+// anton: 8,
 
-};
-
-
-const entries = Object.entries(author);
-
-console.log(entries)
+// };
 
 
-for (const [name, numbers] of entries) {
+// const entries = Object.entries(author);
+
+// console.log(entries)
+
+
+// for (const [name, numbers] of entries) {
     
-    console.log(`${name} ==> ${numbers}`);
+//     console.log(`${name} ==> ${numbers}`);
 
 
-};
+// };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+//     tags: ["premium", "promoted", "top"],
+//     owner: {
+//         name: "Henry",
+//         phone: "982-126-1588",
+//         email: "henry.carter@aptmail.com",
+//     }
+// };
 
 
 
 
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
 
 
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[2]
+
+
+
+// console.log(lastTag);
+
+
+
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+ 
+// for ( const key in apartment){
+//     // console.log(key)
+//     keys.push(key)
+// values.push(apartment[key])
+// }
+// console.log(keys);
+// console.log(values);
+   
+
+
+
+
+// function countProps(object) {
+//   let propCount = 0;
+ 
+//     const keys = Object.keys(object);
+//     console.log(keys);
+//     propCount = keys.length
+
+
+//     console.log(propCount)
+//     return propCount;
+// }
+
+
+
+// countProps({})
+// countProps({ name: "Mango", age: 2 })
+// countProps({ mail: "poly@mail.com", isOnline: true, score: 500 })
+
+
+
+
+
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+
+// const keys = Object.keys(apartment);
+
+
+
+// for (const key of keys ) {
+//     values.push(apartment[key])
+    
+// }
+
+
+
+
+
+// console.log(values)
+
+
+
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+
+// for (const  color  of colors){
+//     // console.log(color)
+   
+//  hexColors.push(color.hex);
+// rgbColors.push(color.rgb);
+  
+// }
+// console.log(hexColors)
+// console.log(rgbColors)
+
+
+
+
+
+
+
+
+
+// // Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). Если продукт с таким названием не найден, функция должна возвращать null.
+
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+//  const fn = function (productName) {
+
+
+
+// for (const product of products) {
+//   if (product.name === productName) {
+//     return product.price;
+//   }
+// }
+// return null;
+
+// }
+
+// console.log(fn("Grip"))
+// console.log(fn("Engine"))
+// console.log(fn("Droid"))
+
+
+
+
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getAllPropValues(propName) {
+
+//   const array = [];
+//   for (const product of products) {
+//     if (product[propName]) {
+//           array.push(product[propName]);
+
+//     }
+//   }
+
+//   return array;
+// }
+
+  
+
+// getAllPropValues()
+
+// console.log(getAllPropValues("quantity"))
+// console.log(getAllPropValues("price"))
+// console.log(getAllPropValues("category"))
+// console.log(getAllPropValues("name"))
+
+
+
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+ 
+
+    for (const product of products) {
+        if (product.name === productName) {
+            return product.price * product.quantity;
+        }
+    }
+}
+
+calculateTotalPrice("Radar")  
+calculateTotalPrice("Droid")  
+calculateTotalPrice("Grip")  
+calculateTotalPrice("Scanner")
 
