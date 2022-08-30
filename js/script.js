2a6ae9db3adc19b7f1a5e6790b25a035abfb5963
Ilman Eldarov Ash-shishani <ilmaneldarov@gmail.com>
2022-08-30 21:26:02 +0300

    3

--- js/script.js
+++ js/script.js
@@ -317,6 +317,26 @@
 
 
 
+const doMath = function (a, b, callback) {
+    const result = callback(a, b);
+
+    console.log(result);
+
+};
+
+const add = function (x, y) {
+    return x + y;
+
+};
+
+const sub = function (x, y) {
+    return x - y
+    
+};
+
+doMath(2, 3, add);
+doMath(10, 8, sub);
+
 
 
 
